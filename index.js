957ced76fec107e7b12bf4b400f2325abfc8e5bc
Thiago <thiagomezcla@gmail.com>
2026-05-06 19:12:49 -0300

    habilitando CORS

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ import { v4 as uuidv4 } from "uuid";
 const app = express();
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  next();
+});
+
 let db;
 
 // 🔥 iniciar banco
